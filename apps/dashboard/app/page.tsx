c729c3f477f8a567438a7afffb5668a90c06934c
JosephT273 <josephtadessem@gmail.com>
2025-07-11 00:24:13 +0300

    feat(config): add global styles and utility functions, update layout and page components

--- apps/dashboard/app/page.tsx
+++ apps/dashboard/app/page.tsx
@@ -1,19 +1,10 @@
-'use client'
-import { useEffect, useState } from 'react'
+'use client';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [message, setMessage] = useState()
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('/api/hello')
-      const { message } = await res.json()
-      setMessage(message)
-    }
-    fetchData()
-  }, [])
-
-  if (!message) return <p>Loading...</p>
-
-  return <p>{message}</p>
+  return (
+    <main className="bg-gray-950 text-white h-screen">
+      <h1>Start hacking</h1>
+    </main>
+  );
 }
